c42890d3035569d3248b49d97b91ccd323ef7306
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2021-01-02 18:10:21 +0100

    index.jsp - donated bags switched with data from DB HomeController modified - model attribute added DonationRepository class modified - bag counting method added data.sql - new DB population inserst for donations table added

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 import java.util.List;
@@ -13,15 +14,20 @@ import java.util.List;
 public class HomeController {
 
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model){
         List<Institution> allInstitutions = institutionRepository.findDistinctFirst4ByOrderByIdDesc();
         model.addAttribute("institutions", allInstitutions);
+
+        Long donatedBags = donationRepository.countAllDonatedBags();
+        model.addAttribute("donatedBags", donatedBags);
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,9 +1,16 @@
 package pl.coderslab.charity.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.entity.Donation;
 
 @Repository
 public interface DonationRepository extends CrudRepository<Donation, Long> {
+
+    @Query(
+            value = "SELECT SUM(quantity) FROM donations;",
+            nativeQuery = true)
+    Long countAllDonatedBags();
+
 }
