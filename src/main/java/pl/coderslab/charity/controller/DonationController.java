6705387167b752002b54f31a6804e48735f194e6
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2021-01-03 00:08:57 +0100

    non JS siplme form working

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -31,7 +31,7 @@ public class DonationController {
         model.addAttribute("categories", allCategories);
 
         model.addAttribute("donation", new Donation());
-        return "donation/form_x";
+        return "donation/form";
     }
 
     @PostMapping("/form")
