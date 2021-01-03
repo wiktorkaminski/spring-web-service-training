fd4dd5db94dacc4c19d174465ea0b3ba12d9b614
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2021-01-03 23:47:55 +0100

    RegistrationController add and HomeController relocation to controller package

--- src/main/java/pl/coderslab/charity/controller/RegistrationController.java
+++ src/main/java/pl/coderslab/charity/controller/RegistrationController.java
@@ -1,2 +1,19 @@
-package pl.coderslab.charity.controller;public class RegistrationController {
+package pl.coderslab.charity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.User;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("user", new User());
+        return "register/form";
+    }
+
 }
