37ff2bed2d0863552b35ead45a9e8dbd1cecc16d
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2021-01-06 17:55:16 +0100

    DonationRepository modified - native SQL query with JPQL replaced

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -8,16 +8,7 @@ import pl.coderslab.charity.entity.Donation;
 @Repository
 public interface DonationRepository extends CrudRepository<Donation, Long> {
 
-    @Query(
-            value = "SELECT SUM(quantity) FROM donations;",
-            nativeQuery = true
-    )
+    @Query("SELECT SUM(d.quantity) FROM Donation d")
     Long countAllDonatedBags();
-
-    @Query(
-            value = "SELECT COUNT(*) FROM donations;",
-            nativeQuery = true
-    )
-    Long countAllDonations();
 
 }
